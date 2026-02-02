4e0b2f7c8d4222358236aff9e44a753453737ab3
Quashawn Hawkins <hawkins.quashawn92@gmail.com>
2026-02-02 05:07:21 -0500

    Force frontend rebuild to fix React errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,4 +3,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Force rebuild - updated 2026-02-02
 createRoot(document.getElementById("root")).render(<App />);
